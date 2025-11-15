2308e1aff3b9f22396287b95c4caef129ece2ebd
Adrian Gjøsund Bjørge <adriangb@stud.ntnu.no>
2025-11-15 09:29:31 +0100

    created more functions for data preprocessing

--- data_reading.py
+++ data_reading.py
@@ -4,6 +4,7 @@ from file_paths import FilePaths
 
 def preprocess_data():
     
+    # Drop attributes we don't care about!
     attributes_we_care_about = get_attributes_we_care_about()
     
     co2 = read_data(FilePaths.CO2.value, attributes_we_care_about)
@@ -18,89 +19,29 @@ def preprocess_data():
     )
     
     final_dataframe = pd.DataFrame(columns=attributes_we_care_about)
-
     
     for country in list(common_countries):
         
+        # only use years which are common in all datasets.
+        # Remove if not all datasets have data for same country.
         common_years = get_common_years_between_dataframes_for_country(
             [co2, fuel_consumption_by_fuel_type, fuel_production],
             country
         )
         
-
-        co2_data = co2[(co2['country'] == country) & (co2['year'].isin(common_years))]
-        consumption = fuel_consumption_by_fuel_type[
-            (fuel_consumption_by_fuel_type['country'] == country) &
-            (fuel_consumption_by_fuel_type['year'].isin(common_years))
-        ]
-        production = fuel_production[
-            (fuel_production['country'] == country) &
-            (fuel_production['year'].isin(common_years))
-        ]
-
-
-        dataframe = pd.DataFrame()
-        dataframe = (
-            co2_data
-            .merge(consumption, on=['country', 'year'], how='inner')
-            .merge(production, on=['country', 'year'], how='inner')
+        merged_data = merge_dataframes_by_common_country_and_years(
+            [co2, fuel_consumption_by_fuel_type, fuel_production],
+            country,
+            common_years
+        )
+        
+        final_dataframe = pd.merge(final_dataframe, 
+            merged_data,
+            how='outer', 
+            on=attributes_we_care_about
         )
 
-        final_dataframe = pd.merge(final_dataframe, dataframe, how='outer', on=attributes_we_care_about)
-        
     return final_dataframe.sort_values(by=["country", "year"])
-
-
-
-    # only use years which are common in all datasets.
-    # Remove if not all datasets have data for same country.
-
-    # Drop attributes we don't care about!
-
-    # Combine into 1 fellse dataset
-
-    # Attribtues we care about:
-
-
-    # year
-    # country / country
-    # oil production - Twh
-    # gas production - Twh
-    # oil consumption - Twh
-    # gas consumption - Twh
-    # co2
-    # gas_co2
-    # oil_co2
-    # population
-    # gdp
-    # co2_growth_abs - rename to co2 growth absolute?
-    # co2_growth_prct - rename to co2 growth percent?
-    # co2_per_unit_energy
-    # total_ghg
-    # cumulative_oil_co2
-    # methane
-
-    # maybe
-    # gas_co2_per_capita
-    # oil_co2_per_capita
-    # cumulative_co2
-    # cumulative_gas_co2
-    # flaring_co2
-    # cumulative_flaring_co2
-    # energy_per_capita
-    # energy_per_gdp
-    # share_global_co2
-    # share_global_cumulative_co2
-    # share_global_cumulative_flaring_co2
-    # share_global_cumulative_gas_co2
-    # share_global_cumulative_oil_co2
-    # share_global_flaring_co2
-    # share_global_gas_co2
-    # share_global_oil_co2
-    # share_of_temperature_change_from_ghg
-    # temperature_change_from_ch4
-    # temperature_change_from_co2
-    # temperature_change_from_ghg
 
 def read_data(path, relevant_attributes = []):
     print(path)    
@@ -110,7 +51,95 @@ def read_data(path, relevant_attributes = []):
 
     return data
 
+def get_common_years_between_dataframes_for_country(dataframes, country):
+    
+    all_years = []
+    
+    for dataframe in dataframes:
+        if 'year' not in dataframe:
+            raise ValueError("Dataframe does not contain 'year' column")
+        if 'country' not in dataframe:
+            raise ValueError("Dataframe does not contain 'country' column")
+        years = set(dataframe[dataframe['country'] == country]['year'])
+        all_years.append(years)
 
+    common_years = set.intersection(*all_years)
+    return common_years
+
+def merge_dataframes_by_common_country_and_years(dataframes, country, common_years):
+    
+    attributes_we_care_about = get_attributes_we_care_about()
+    final_dataframe = pd.DataFrame(columns=attributes_we_care_about)
+    
+    filtered_dataframes = []
+    
+    for dataframe in dataframes:
+        if 'year' not in dataframe:
+            raise ValueError("Dataframe does not contain 'year' column")
+        if 'country' not in dataframe:
+            raise ValueError("Dataframe does not contain 'country' column")
+        
+        filtered = dataframe[
+            (dataframe['country'] == country) &
+            (dataframe['year'].isin(common_years))
+        ]
+        
+        filtered_dataframes.append(filtered)
+        
+    if not filtered_dataframes:
+        return final_dataframe
+    
+    final_dataframe  = filtered_dataframes[0]
+    
+    for dataframe in filtered_dataframes[1:]:
+        final_dataframe = final_dataframe.merge(dataframe, on=['country', 'year'], how='inner')
+
+    return final_dataframe
+
+
+
+
+
+# Attribtues we care about:
+# year
+# country / country
+# oil production - Twh
+# gas production - Twh
+# oil consumption - Twh
+# gas consumption - Twh
+# co2
+# gas_co2
+# oil_co2
+# population
+# gdp
+# co2_growth_abs - rename to co2 growth absolute?
+# co2_growth_prct - rename to co2 growth percent?
+# co2_per_unit_energy
+# total_ghg
+# cumulative_oil_co2
+# methane
+
+# maybe
+# gas_co2_per_capita
+# oil_co2_per_capita
+# cumulative_co2
+# cumulative_gas_co2
+# flaring_co2
+# cumulative_flaring_co2
+# energy_per_capita
+# energy_per_gdp
+# share_global_co2
+# share_global_cumulative_co2
+# share_global_cumulative_flaring_co2
+# share_global_cumulative_gas_co2
+# share_global_cumulative_oil_co2
+# share_global_flaring_co2
+# share_global_gas_co2
+# share_global_oil_co2
+# share_of_temperature_change_from_ghg
+# temperature_change_from_ch4
+# temperature_change_from_co2
+# temperature_change_from_ghg
 def get_attributes_we_care_about():
     return [
     "year",
@@ -131,19 +160,3 @@ def get_attributes_we_care_about():
     "cumulative_oil_co2",
     "methane",
 ]
-
-
-def get_common_years_between_dataframes_for_country(dataframes, country):
-    
-    all_years = []
-    
-    for dataframe in dataframes:
-        if 'year' not in dataframe:
-            raise ValueError("Dataframe does not contain 'year' column")
-        if 'country' not in dataframe:
-            raise ValueError("Dataframe does not contain 'country' column")
-        years = set(dataframe[dataframe['country'] == country]['year'])
-        all_years.append(years)
-
-    common_years = set.intersection(*all_years)
-    return common_years
