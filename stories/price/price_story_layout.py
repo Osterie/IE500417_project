56d34cd440fdcce3082808a8331763055f63cf88
Adrian Gjøsund Bjørge <adriangb@stud.ntnu.no>
2025-12-07 09:09:06 +0100

    Mentioned oil/gas dependent/producing countries for price story title

--- stories/price/price_story_layout.py
+++ stories/price/price_story_layout.py
@@ -53,4 +53,4 @@ def create_price_story_layout(processed_data):
     )
 
 def create_price_title():
-    return html.H1("Correlation between oil and gas prices with consumption and production", style={"textAlign": "center"})
+    return html.H1("Correlation between oil and gas prices with consumption and production for oil and gas producing/dependent countries", style={"textAlign": "center"})
